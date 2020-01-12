7260ddecba01e863391ed32c9abaae9b70135c04
kso1 <kso@corcode.com>
2020-01-12 19:58:11 +0100

    -a

---
+++ refrigerator-tracker/app.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'sinatra'
+require 'yaml/store'
+
+# configure do 
+  # set :public_folder, File.dirname(__FILE__) + '/static'
+    # set :views, Proc.new { File.join(root, "templates") }
+# end
+
+get '/' do
+    @title = 'Welcome to the Suffragist!'
+    erb :index
+end
+
+post '/cast' do
+  @title = 'Thanks for casting your vote!'
+  @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
+  erb :cast
+end
+
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
+end
+
+Choices = {
+  'HAM' => 'Hamburger',
+  'PIZ' => 'Pizza',
+  'CUR' => 'Curry',
+  'NOO' => 'Noodles',
+}
